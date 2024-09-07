ef2f39c2cc389aae457b3b951f95816bf6d91acc
Socrate-bit <85810474+Socrate-bit@users.noreply.github.com>
2024-09-07 03:04:43 +0200

    Commit 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tracker_v1/screens/auth.dart';
 import 'package:tracker_v1/screens/navigation.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 // import 'package:tracker_v1/colors.dart';
 
 final kcolorTheme = const ColorScheme.dark().copyWith(
@@ -13,6 +17,9 @@ final kcolorTheme = const ColorScheme.dark().copyWith(
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -28,7 +35,15 @@ class MyApp extends StatelessWidget {
           iconTheme: const IconThemeData().copyWith(color: Colors.grey),
           inputDecorationTheme:
               const InputDecorationTheme().copyWith(fillColor: Colors.grey)),
-      home: const MainScreen(),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (ctx, snapshot) {
+            if (snapshot.hasData) {
+              return const MainScreen();
+            } else {
+              return const AuthScreen();
+            }
+          }),
     );
   }
 }
--- lib/screens/new_habit.dart
+++ lib/screens/new_habit.dart
@@ -5,7 +5,7 @@ import 'package:flutter_iconpicker/flutter_iconpicker.dart';
 import 'package:tracker_v1/providers/habits_provider.dart';
 import 'package:tracker_v1/widgets/circle_day.dart';
 import 'package:tracker_v1/models/habit.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 
 class NewHabitScreen extends ConsumerStatefulWidget {
   const NewHabitScreen({super.key});
@@ -81,7 +81,7 @@ class _MainScreenState extends ConsumerState<NewHabitScreen> {
     }
 
     Habit newHabit = Habit(
-        userId: '123456789',
+        userId: FirebaseAuth.instance.currentUser!.uid,
         icon: _enteredIcon,
         name: _enteredName!,
         description: _entereDescription,
